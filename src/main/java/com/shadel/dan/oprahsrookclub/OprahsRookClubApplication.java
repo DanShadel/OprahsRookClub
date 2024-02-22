ed91fc3380d0663ad994fe092686f49abaf2cc89
Dan Shadel <shadeldaniel@gmail.com>
2024-02-22 13:34:58 -0700

    init spring commit

---
+++ src/main/java/com/shadel/dan/oprahsrookclub/OprahsRookClubApplication.java
@@ -0,0 +1,13 @@
+package com.shadel.dan.oprahsrookclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OprahsRookClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OprahsRookClubApplication.class, args);
+	}
+
+}
